4df460d25fee627be85db667ab9e1b522d388130
osbm <osmanfbayram@gmail.com>
2024-10-26 00:37:23 +0300

    only beautifying remains

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,26 @@
 import { defineConfig } from 'astro/config';
 
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://osbm.dev',
     trailingSlash: 'never',
     output: 'static',
+    // i18n: {
+    //     defaultLocale: "en",
+    //     locales: ["en", "tr", "ja"],
+    // },
+    integrations: [
+        mdx(
+            {
+                remarkPlugins: [remarkMath],
+                rehypePlugins: [rehypeKatex],
+            }
+        ),
+        sitemap(),
+    ],
 });
