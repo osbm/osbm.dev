f461929b70f7218252eceb4bb4034eea413564f7
osbm <osmanfbayram@gmail.com>
2025-03-16 21:11:45 +0300

    add devshell

--- flake.nix
+++ flake.nix
@@ -1,21 +1,21 @@
 {
   description = "osbm.dev astro website";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { nixpkgs, ...}:
+  outputs =
+    { nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in
     {
-      defaultPackage.x86_64-linux = pkgs.stdenv.mkDerivation {
-        name = "osbm.dev";
-        src = ./.;
-        buildInputs = [ pkgs.nodejs ];
-        installPhase = ''
-          mkdir -p $out
-          cp -r * $out
+      devShells."${system}".default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs
+        ];
+        shellHook = ''
+          echo Start developing the osbm.dev
         '';
       };
-    };
 
+    };
 }
