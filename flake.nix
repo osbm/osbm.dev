77537fd0eacc6eb9082f527507e6f3d40a70909c
osbm <osmanfbayram@gmail.com>
2025-04-22 15:26:07 +0300

    formatting

--- flake.nix
+++ flake.nix
@@ -1,44 +1,32 @@
 {
   description = "osbm.dev astro website";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs =
-    { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      package-json = builtins.fromJSON (builtins.readFile ./package.json);
-    in
-    {
-      devShells."${system}".default = pkgs.mkShell {
-        packages = with pkgs; [
-          nodejs
-          nodePackages.npm
-        ];
-        shellHook = ''
-          export ASTRO_TELEMETRY_DISABLED=1
-          echo Start developing the osbm.dev
-        '';
-      };
-      packages."${system}".default = pkgs.buildNpmPackage rec {
-        pname = package-json.name;
-        version = package-json.version;
-        src = ./.;
-
-        buildInputs = with pkgs; [
-          nodejs
-          # npmHooks.npmConfigHook
-        ];
-        installPhase = ''
-          runHook preInstall
-          cp -pr --reflink=auto dist $out/
-          runHook postInstall
-        '';
-        # npmDeps = pkgs.importNpmLock {
-        #   npmRoot = ./.;
-        # };
-        # npmConfigHook = pkgs.importNpmLock.npmConfigHook;
-        npmDepsHash = "sha256-Jvwu4F/ruaOqL+YDPoZ6XOZVOl3Ug+YFrK8VjuwD2W8=";
-
-      };
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+    package-json = builtins.fromJSON (builtins.readFile ./package.json);
+  in {
+    devShells."${system}".default = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs
+        nodePackages.npm
+      ];
+      shellHook = ''
+        export ASTRO_TELEMETRY_DISABLED=1
+        echo Start developing the osbm.dev
+      '';
     };
+    packages."${system}".default = pkgs.buildNpmPackage {
+      pname = package-json.name;
+      version = package-json.version;
+      src = ./.;
+      buildInputs = [pkgs.nodejs];
+      installPhase = ''
+        runHook preInstall
+        cp -pr --reflink=auto dist $out/
+        runHook postInstall
+      '';
+      npmDepsHash = "sha256-Jvwu4F/ruaOqL+YDPoZ6XOZVOl3Ug+YFrK8VjuwD2W8=";
+    };
+  };
 }
